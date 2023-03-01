ae53184213ce69c04009ff1951d941c1f92b477c
Banelord <chuchuk.tima@mail.ru>
2023-03-01 15:07:52 +0300

    postman fix 3

--- src/main/java/ru/practicum/shareit/request/model/Request.java
+++ src/main/java/ru/practicum/shareit/request/model/Request.java
@@ -5,6 +5,7 @@ import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * TODO Sprint add-item-requests.
@@ -27,4 +28,22 @@ public class Request {
     private User user;
     @Column(nullable = false)
     private LocalDateTime created;
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Request that = (Request) o;
+
+        if (!Objects.equals(id, that.id)) return false;
+        if (!description.equals(that.description)) return false;
+        if (!user.equals(that.user)) return false;
+        return id != null && id.equals(((Request) o).getId());
+    }
 }
